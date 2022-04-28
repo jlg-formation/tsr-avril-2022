1aaeae1874106dc615d65329ae8093475c2c6e91
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-28 12:21:19 +0200

    dev: added generateCircle

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,14 @@
-let nbr: number = 123;
-nbr = 12;
-console.log("nbr: ", nbr);
-
 console.log("start");
+
+const generateCircle = (x: number, y: number, r: number) => {
+  const svgns = "http://www.w3.org/2000/svg";
+  const container = document.querySelector("svg g.points");
+
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", x + "");
+  circle.setAttributeNS(null, "cy", y + "");
+  circle.setAttributeNS(null, "r", r + "");
+  container.appendChild(circle);
+};
+
+generateCircle(0, 0, 45);
