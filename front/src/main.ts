bb983b4cbea77e6c8c813264ed381dfd21e1b3d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-28 14:40:22 +0200

    dev: installed webpack

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,3 +1,5 @@
+import { Point } from "./interfaces/Point";
+
 console.log("start");
 
 const generateNCircles = (n = 10) => {
@@ -16,6 +18,9 @@ const generateNCircles = (n = 10) => {
 const generateCircle = (x: number, y: number, r: number) => {
   const svgns = "http://www.w3.org/2000/svg";
   const container = document.querySelector("svg g.points");
+  if (container === null) {
+    throw new Error("cannot find svg g.points");
+  }
 
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x + "");
@@ -23,5 +28,30 @@ const generateCircle = (x: number, y: number, r: number) => {
   circle.setAttributeNS(null, "r", r + "");
   container.appendChild(circle);
 };
+
+const generateLine = (p1: Point, p2: Point) => {
+  const svgns = "http://www.w3.org/2000/svg";
+  const container = document.querySelector("svg g.lines");
+  if (container === null) {
+    throw new Error("cannot find svg g.lines");
+  }
+
+  const line = document.createElementNS(svgns, "line");
+  line.setAttributeNS(null, "x1", p1.x + "");
+  line.setAttributeNS(null, "y1", p1.y + "");
+  line.setAttributeNS(null, "x2", p2.x + "");
+  line.setAttributeNS(null, "y2", p2.y + "");
+  container.appendChild(line);
+};
+
+const p1: Point = {
+  x: 23,
+  y: 12,
+};
+const p2: Point = {
+  x: 120,
+  y: 340,
+};
+generateLine(p1, p2);
 
 generateNCircles();
