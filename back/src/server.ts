a184a09d680d9e9852fbd37ebd8bcf3ecbab6299
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-29 14:26:16 +0200

    dev: added serve-index

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import serveIndex from "serve-index";
 
 console.log("about to start the server");
 const app = express();
 const port = 3000;
+const wwwDir = "./public";
 
-app.get("/", (req, res) => {
-  res.json({ date: new Date() });
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
 });
+
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
