e86208871d98db58fd5ba2175c0cad38f6372b68
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-29 15:33:10 +0200

    dev: isLoading

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,16 +3,30 @@ import "./style.scss";
 import { Command } from "./Command";
 import { Figure } from "./Figure";
 import { Api } from "./Api";
+import { getContainer } from "./utils";
 
 console.log("start");
+
+const hideCommand = () => {
+  getContainer("div.command").setAttribute("hidden", "hidden");
+  getContainer("div.loading").removeAttribute("hidden");
+};
+
+const showCommand = () => {
+  getContainer("div.command").removeAttribute("hidden");
+  getContainer("div.loading").setAttribute("hidden", "hidden");
+};
 
 const main = async () => {
   const figure = new Figure();
   const api = new Api();
+
+  hideCommand();
   const defaultConfig = await api.getConfig();
+  showCommand();
+  const command = new Command(defaultConfig);
   figure.draw(defaultConfig);
 
-  const command = new Command(defaultConfig);
   command.onUpdate((config) => {
     figure.draw(config);
   });
